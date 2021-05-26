637b01c9088646246f897a03d5f1ed33e9e7b0c4
ramossis <44950475+ramossis@users.noreply.github.com>
2021-05-26 18:28:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var nodos = null;
+var aristas = null;
+
+function fo() {
+    nodos = document.getElementsByName("nodos")[0].value;
+    aristas = document.getElementsByName("aristas")[0].value;
+
+    console.log(nodos, aristas);
+};
